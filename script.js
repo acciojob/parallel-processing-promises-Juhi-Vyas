f9e55aa9cefd66ef98de758e92d7d8ab094e4a38
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2025-12-27 16:58:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
+const error = document.getElementById("error");
 const btn = document.getElementById("download-images-button");
 
 const images = [
@@ -7,3 +7,25 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click", downloadImages)
+
+let p1 = fetch(image[0].url)
+let p2 = fetch(image[1].url)
+let p3 = fetch(image[2].url)
+
+function downloadImages() {
+	//blob -> converts a response into binary data
+	
+		Promise.all([p1,p2,p3])
+		.then(res => res.blob())
+		.then(blob => {
+			let img = document.crateElement(img)
+			img.src = URL.createObjectURL(blob)
+			output.appendChild(img)
+		})
+		.catch(err => {
+			error.innerText = "Error: " + err})
+}
+
+
